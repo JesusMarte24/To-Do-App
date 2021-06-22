368313b5544586f48eddace2cd589610c74e8d5e
Jesus Marte <jesusmarte2430@gmail.com>
2021-06-21 20:33:45 -0400

    Litle Changes to header

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -2,13 +2,23 @@ import React from 'react';
 import { ToDo } from './components/ToDo';
 
 export const ToDoApp = () => {
+	const inputValue = document.getElementById('input--value');
+	const handleSubmit = (e) => {
+		e.preventDefault();
+	};
+
+	const handleNewTask = () => {
+		console.log(inputValue.value);
+	};
+
 	return (
 		<>
 			<header className="main--header">
 				<h1>My Day</h1>
 				<h2>Monday</h2>
+				<span>06/21/2021</span>
 			</header>
-			<form className="main--wrapper" id="main--form">
+			<form className="main--wrapper" id="main--form" onSubmit={handleSubmit}>
 				<div className="wrapper--top">
 					<h3 className="bi bi-arrow-repeat btn btn-outline-dark"> Reset</h3>
 				</div>
@@ -17,11 +27,17 @@ export const ToDoApp = () => {
 				</div>
 			</form>
 			<section className="section--input">
-				<input type="text" form="main--form" placeholder="Add New Task..." />
+				<input
+					id="input--value"
+					type="text"
+					form="main--form"
+					placeholder="Add New Task..."
+				/>
 				<button
 					type="submit"
 					form="main--form"
 					className="bi bi-clipboard-plus btn"
+					onClick={handleNewTask}
 				></button>
 			</section>
 		</>
