5f41f10d9fcb149b7024294508c4c5efce8b7123
Jesus Marte <jesusmarte2430@gmail.com>
2021-06-22 15:06:39 -0400

    Create has Been Added (CRUD)

--- src/ToDoApp.js
+++ src/ToDoApp.js
@@ -1,29 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ToDo } from './components/ToDo';
+import { uid } from 'uid';
 
 export const ToDoApp = () => {
 	const inputValue = document.getElementById('input--value');
+	const [todos, setTodos] = useState(JSON.parse(localStorage.getItem("todos")) || []);
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
 	};
 
 	const handleNewTask = () => {
-		console.log(inputValue.value);
+		setTodos([
+			...todos,
+			{
+				id: uid(),
+				task: inputValue.value,
+			},
+		]);
+		
 	};
 
+	//Esto funciona porque cada vez que se actualizan los "todos" las dependencias de
+	//useEffect, este dispara todas las acciones que estan dentro de el.
+	useEffect(() => {
+		const storageTodos = JSON.stringify(todos);
+	    localStorage.setItem('todos', storageTodos);
+	}, [todos])
+
+	const handleReset = () => {
+		setTodos([]);
+		localStorage.clear();
+	};
+
+	
 	return (
 		<>
 			<header className="main--header">
-				<h1>My Day</h1>
+				<h1>To Do List</h1>
 				<h2>Monday</h2>
 				<span>06/21/2021</span>
 			</header>
 			<form className="main--wrapper" id="main--form" onSubmit={handleSubmit}>
 				<div className="wrapper--top">
-					<h3 className="bi bi-arrow-repeat btn btn-outline-dark"> Reset</h3>
+					<h3 onClick={handleReset} className="bi bi-arrow-repeat btn btn-outline-dark">
+						|Reset
+					</h3>
 				</div>
 				<div className="main--content">
-					<ToDo />
+					<ToDo todoValues={todos} />
 				</div>
 			</form>
 			<section className="section--input">
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
-export const ToDo = () => {
+export const ToDo = ({ todoValues }) => {
+
+	console.log(todoValues);
+
 	return (
-		<div className="content--todo">
-			<p>Escuchar musica de el cherry</p>
-			<div>
-				<i className="bi bi-check2-circle btn btn-outline-primary"></i>
-				<i className="bi bi-pencil btn btn-outline-dark"></i>
-				<i className="bi bi-trash btn btn-outline-danger"></i>
-			</div>
-		</div>
+		<>
+		{
+			todoValues.map(todo => {
+
+			return <div className="content--todo" key={todo.id}>
+						<p>{todo.task}</p>
+						<div>
+							<i className="bi bi-check2-circle btn btn-outline-primary"></i>
+							<i className="bi bi-pencil btn btn-outline-dark"></i>
+							<i className="bi bi-trash btn btn-outline-danger"></i>
+						</div>
+					</div>
+			})
+		}
+		
+		</>
 	);
 };
